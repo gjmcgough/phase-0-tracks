cf667f0cb586b36a823fa14097ff48751b2314c6
Greg McGough <gjmcgough@gmail.com>
2016-06-18 05:34:28 -0700

    added decrypt method. need to be able to access each char individually

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -11,13 +11,35 @@ def encrypt(string)
 	end
 end
 
-encrypt("abcd")
+
+encrypt("abc")
+# should return "bcd"
+encrypt("zed")
+# should return "afe"
 
 
 # define a decrypt method
 # create loop for string length
-# reverse letters 
+# reverse letters
+# use logic to single out each letter then print the letter before it
+# create string ab..yz and set each index to index - 1
+# access each character individually
 
 def decrypt(str)
-	index = 0
-	while index < string.length
+	num = 0
+	while num < str.length
+		dec = ("abcdefghijklmnopqrstuvwxyz".index("#{str.chr}")) - 1
+		puts "abcdefghijklmnopqrstuvwxyz"[dec]
+		num += 1
+	end
+end
+
+decrypt("b")
+decrypt("c")
+decrypt("d")
+decrypt("a")
+decrypt("f")
+decrypt("e")
+
+# abc
+# zed
