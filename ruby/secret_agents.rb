c20c8a80418c162555fbb0782c68d8a236090d51
Greg McGough <gjmcgough@gmail.com>
2016-06-17 17:14:59 -0700

    created encryption program

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,23 @@
+# define method encrypt
+# create loop for string length
+# advance letters with += and index
+
+
+def encrypt(string)
+	index = 0
+	while index < string.length
+		puts string[index].next
+		index += 1
+	end
+end
+
+encrypt("abcd")
+
+
+# define a decrypt method
+# create loop for string length
+# reverse letters 
+
+def decrypt(str)
+	index = 0
+	while index < string.length
