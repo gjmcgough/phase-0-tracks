fa5b286274d9142b9fb66832fff2281c669fa0a0
Greg McGough <gjmcgough@gmail.com>
2016-06-19 09:23:55 -0700

    updated and fixed bugs in encrypt and decrypt

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,20 +1,42 @@
+# create interface, ask whether encrypt or decrypt password
+# asks for password
+# conducts operation, prints results, exits
+puts "Would you like to encrypt or decrypt a password?"
+choice = gets.chomp
+
+puts "Enter password:"
+password = gets.chomp
+
+loop = false
+until loop
+	if choice == "encrypt"
+		p encrypt(password)
+		loop = true
+	elsif choice == "decrypt"
+		p decrypt(password)
+		loop = true
+	else
+		puts "I don't understand"
+
 # define method encrypt
 # create loop for string length
 # advance letters with += and index
 
 
 def encrypt(string)
-	index = 0
-	while index < string.length
-		if string[index].next.include? "aa"
-			puts string[index].next.gsub("aa" , "a")
-		else
-			puts string[index].next
-		end
-		index += 1
-	end
+    index = 0
+    new_str = ""
+    while index < string.length
+        if string[index].next.include? "aa"
+            new_str += print string[index].next.gsub("aa" , "a")
+        else
+             new_str += string[index].next
+        end
+         
+        index += 1
+    end
+    return new_str
 end
-
 
 encrypt("abc")
 # should return "bcd"
@@ -29,21 +51,21 @@ encrypt("zed")
 # create string ab..yz and set each index to index - 1
 # access each character individually
 
+
 def decrypt(str)
-	num = 0
-	while num < str.length
-		dec = ("abcdefghijklmnopqrstuvwxyz".index("#{str}")) - 1
-		puts "abcdefghijklmnopqrstuvwxyz"[dec]
-		num += 1
-	end
+    idx = 0
+    new_str = ""
+    while idx < str.length
+        dec = ("abcdefghijklmnopqrstuvwxyz".index(str[idx])) - 1
+        new_str += "abcdefghijklmnopqrstuvwxyz"[dec]
+        idx += 1
+    end
+    return new_str
 end
 
-decrypt("b")
-decrypt("c")
-decrypt("d")
-decrypt("a")
-decrypt("f")
-decrypt("e")
+
+
+decrypt("bcd")
 
 # abc
 # zed
