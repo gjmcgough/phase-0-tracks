9871ab6dc598036284f51046e8a57619f3940d03
Greg McGough <gjmcgough@gmail.com>
2016-06-22 12:13:29 -0700

    added surfer array and hash and ran . each and .map on them

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -3,4 +3,36 @@ def block_method
 	3.times { yield }
 end
 
-block_method { |i| puts "printing block #{i}"}
+block_method { puts "printing block"}
+
+
+surfer_array = ["john john", "dane", "clay", "filipe"]
+
+surfer_attribute_hash = {
+	john_john: "heavy barrels",
+	dane: "freesurfing",
+	clay: "huge laybacks",
+	filipe: "flawless rotating airs"
+}
+
+puts "Original Data"
+p surfer_array
+p surfer_attribute_hash
+
+
+puts "After .each Call"
+surfer_array.each do |surfer|
+	puts surfer.reverse
+end
+
+surfer_attribute_hash.each do |surfer, attribute|
+	puts "#{surfer} excels when it comes to #{attribute}"
+end
+
+puts "After .map! Call"
+
+surfer_array.map! do |surfer|
+	surfer.upcase
+end
+
+p surfer_array
