d0d5201833ea5640489c444742ca4b77b2b48cfa
Greg McGough <gjmcgough@gmail.com>
2016-06-23 14:04:12 -0700

    added nested data

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,26 @@
+surf_and _skate_store = {
+	hardware: {
+		surf: [
+			'surfboards', 
+			'fins', 
+			'leashes',
+			'wax'
+			]
+		skate: [
+			'skateboards',
+			'trucks',
+			'bearings',
+			'wheels',
+			'grip tape'
+			]
+		}
+	clothes: [
+		'boardshorts',
+		't-shirts',
+		'skate pants',
+		'flannels',
+		'hats',
+		'shoes',
+		'sandals'
+		]
+}
