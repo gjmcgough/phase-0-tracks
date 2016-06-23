59a9462a9e4ff1c974da85abf99d4f2cb5896b35
Greg McGough <gjmcgough@gmail.com>
2016-06-23 15:12:46 -0700

    added name swap to alias manager

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,10 @@
+# Swapping the first and last name.
+
+def name_swap(first_name, last_name)
+	swap = last_name + " " + first_name
+end
+
+name_swap("Greg", "McGough")
+# Changing all of the vowels (a, e, i, o, or u) to the next vowel in 'aeiou', and all of the consonants 
+# (everything else besides the vowels) to the next consonant in the alphabet. 
+# So 'a' would become 'e', 'u' would become 'a', and 'd' would become 'f'.
