766d2da1852f0bc3495c7c5cf420ae2d8b110d75
Greg McGough <gjmcgough@gmail.com>
2016-06-27 15:29:22 -0700

    added pseudo code to gps 2.2

---
+++ ruby/gps2_2.rb
@@ -0,0 +1,34 @@
+# Method to create a list
+# input: string of items separated by spaces (example: "carrots apples cereal pizza") 
+# steps: 
+  # separate string into items
+  # make each item a key
+  # set default quantity
+  # print the list to the console [can you use one of your other methods here? come back and use last method here]
+# output: hash
+
+# Method to add an item to a list
+# input: item name and optional quantity
+# steps: 
+	# add key value pair to hash
+		# add item quantity pair to list
+# output: updated hash with new item
+
+# Method to remove an item from the list
+# input: item name to be removed
+# steps: 
+	# use nested data structures to access item to be removed
+	# remove item from list
+# output: updated hash without deleted item
+
+# Method to update the quantity of an item
+# input: item and number of items added pair
+# steps:
+	# use nested data structures to access the pair and update the quantity
+# output: updated hash with new quantity
+
+# Method to print a list and make it look pretty
+# input: hash to be printed
+# steps:
+	# use interpolation to print each pair with a string
+# output: print hash in a string with item; quantity
