f44ba4d3740c4d6503dfe5ffcda18b8afe8ed941
Greg McGough <gjmcgough@gmail.com>
2016-06-27 07:34:59 -0700

    final calc

--- calculator.rb
+++ calculator.rb
@@ -1,20 +1,43 @@
 # write code that accepts 3 values, 2 ints and 1 '+'
 
+
 def calculator(input)
-	input.split('')
+	input = input.split(' ')
 	a = input[0].to_i
 	b = input[1]
 	c = input[2].to_i
-	a.send(b, c)
+	return a.send(b, c)
 end
+
+
 
 #p calculator(4, '+', 5)
 #p calculator(4, '-', 5)
 #p calculator(4, '*', 5)
 #p calculator(4, '/', 5)
 
+result_hsh = Hash.new{'result hash'}
+
+counter = 0
+loop = false
+until loop
+
+
 puts 'Please enter calculation'
 input = gets.chomp
 
+result_hsh[input] =  calculator(input)
 
-p calculator(input)
+puts calculator(input)
+
+
+puts 'If you would like to exit please type "done" otherwise hit "enter" to start a new calculation.'
+exit = gets.chomp
+counter += 1
+
+	if exit == 'done'
+		puts counter.to_s + ' ' + 'calculations done'
+		puts result_hsh
+		loop = true
+	end
+end
