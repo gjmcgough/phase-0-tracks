7fb6040a66214449acbef85b7411756e730201b3
Greg McGough <gjmcgough@gmail.com>
2016-07-17 08:23:55 -0700

    7.1 complete

---
+++ js/explore.js
@@ -0,0 +1,16 @@
+// access the letters in the string
+// switch the order for each letter
+// print new reversed string
+
+var str = "hello"
+
+var newStr = ""
+var strIndexLength = str.length - 1
+for (var i = strIndexLength; i >= 0; i--) {
+	newStr += (str[i]);
+}
+
+
+if (1 == 1)
+	console.log(newStr)
+
