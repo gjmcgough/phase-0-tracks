2c72e70a178f0d883241fd4e41f97a320cd074f3
Greg McGough <gjmcgough@gmail.com>
2016-07-25 07:32:24 -0700

    complete 8.5 with UI

--- databases/contacts_customers/customers_contacts.rb
+++ databases/contacts_customers/customers_contacts.rb
@@ -1,7 +1,8 @@
 require 'sqlite3'
-require 'faker'
+# require 'faker'
 
 db = SQLite3::Database.new("customers.db")
+db.results_as_hash = true
 
 create_table = <<-SQL
   CREATE TABLE IF NOT EXISTS customers(
@@ -12,4 +13,37 @@ create_table = <<-SQL
   )
 SQL
 
-db.execute(create_table)
+def add_customer(db, business_name, primary_contact_name, phone)
+	db.execute("INSERT INTO customers (business_name, primary_contact_name, phone) VALUES (?, ?, ?)", [business_name, primary_contact_name, phone])
+end
+
+
+# UI
+loop do
+puts "Would you like to add a customer? (y/n)"
+new_customer = gets.chomp
+	if new_customer == "n"
+		break
+	elsif new_customer == "y"
+		puts "What is the customer business name?"
+		business_name = gets.chomp
+		puts "What is the customer primary contact name?"
+		primary_contact_name = gets.chomp
+		puts "What is the customer phone number?"
+		phone = gets.to_i
+		add_customer(db, business_name, primary_contact_name, phone)
+	else
+		puts "I did not understand."
+	end
+end
+
+
+
+# DRIVER CODE
+
+# db.execute(create_table)
+# db.execute("INSERT INTO customers (business_name, primary_contact_name, phone) VALUES ('Coconut Peets', 'Peet', 6195556789)")
+# customers = db.execute("SELECT * FROM customers")
+# puts customers.class
+# p customers
+# add_customer(db, "Joe Roper's Ding Repair", "Joe Roper", 8582134214)
